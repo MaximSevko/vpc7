e8511ec4c8cdf4889cf72401b4f05213d2a3ff58
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-07 20:16:37 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,12 +7,3 @@ variable "aws_region" {
 #  default = "fd00:10::/56"
 #}
 
-# Define the IPv6 CIDR blocks for private subnets
-variable "private_subnet_cidr_blocks" {
-  default = ["fd00:10:1::/64", "fd00:10:2::/64", "fd00:10:3::/64"]
-}
-
-# Define the IPv6 CIDR blocks for intra subnets
-variable "intra_subnet_cidr_blocks" {
-  default = ["fd00:20:1::/64", "fd00:20:2::/64", "fd00:20:3::/64"]
-}
