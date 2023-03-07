ca8b7d39cbfefc9196ff186b1272d3138703a140
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-07 20:35:37 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 
 # Define the VPC and subnet CIDR blocks
 variable "vpc_cidr_block" {
-  default = "2001:db8:1234:1a00::/56"
+  default = "2001:db8:1234:::/56"
 }
 
 # Define the IPv6 CIDR blocks for private subnets
