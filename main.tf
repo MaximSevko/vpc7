b45917a227cd21acbe41831c20e5a9e1407580ee
Maxim <72321151+MaximSevko@users.noreply.github.com>
2023-03-07 19:48:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "private_subnet" {
   count                  = length(var.private_subnet_cidr_blocks)
   cidr_block             = var.private_subnet_cidr_blocks[count.index]
-  vpc_id                 = aws_vpc.main.id
+  vpc_id                 = aws_vpc.vpc7.id
   assign_ipv6_address_on_creation = true
 }
 
@@ -35,12 +35,12 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "intra_subnet" {
   count                  = length(var.intra_subnet_cidr_blocks)
   cidr_block             = var.intra_subnet_cidr_blocks[count.index]
-  vpc_id                 = aws_vpc.main.id
+  vpc_id                 = aws_vpc.vpc7.id
   assign_ipv6_address_on_creation = true
 }
 
 # Associate the private subnets with the egress-only gateway
-resource "aws_subnet_ipv6_cidr_block_association" "private" {
+resource "aws_subnet_association" "private" {
   count           = length(var.private_subnet_cidr_blocks)
   subnet_id       = aws_subnet.private_subnet[count.index].id
   ipv6_cidr_block = aws_subnet.private_subnet[count.index].ipv6_cidr_block_association[0].ipv6_cidr_block
